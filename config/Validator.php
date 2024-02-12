345ee886c08a8aa23a3268e3617b13ef7c3982ee
Jorge Armando Ceras Cárdenas <jorge.armando.c.cardenas@gmail.com>
2024-02-11 21:45:42 -0600

    Se ha creado la clase Validator que servirá para validar datos

---
+++ config/Validator.php
@@ -0,0 +1,6 @@
+<?php
+// Clase para la validación de los datos, el objetivo principal de esta es mantener el código limpio y fácil de modificar en caso de que se necesite cambiar el método de validación.
+class Validator
+{
+  // TODO: Agregar validaciones para los datos, uno por uno y una función para validar todos los datos acorde a la operación que se vaya a realizar.
+}
