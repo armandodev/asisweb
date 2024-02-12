1523026608898210c18b0243e305288a270558d6
Jorge Armando Ceras Cárdenas <jorge.armando.c.cardenas@gmail.com>
2024-02-11 21:44:39 -0600

    Se ha creado la clase Database Se han agregado las variables de la base de datos Se ha creado la función de conexión Se ha creado la función para la desconectar la base de datos Se ha creado la función para ejecutar una consulta Se le ha agregado seguridad contra inyección de SQL

---
+++ config/Database.php
@@ -0,0 +1,51 @@
+<?php
+// Clase para el manejo total de la base de datos, esta se crea con el objetivo que, en caso de que busquen cambiar cualquier tema de consultas o de conexión (entre otros), solo se modifique en un solo lugar.
+class Database
+{
+  // Variables para la conexión a la base de datos.
+  private $host = 'localhost';
+  private $db_name = 'asisweb';
+  private $username = 'root';
+  private $password = '';
+  private $port = '3306';
+  public $conn;
+
+  // Función para la conexión a la base de datos.
+  public function getConnection()
+  {
+    // Si la conexión no existe, la crea.
+    $this->conn = null;
+
+    try {
+      // Intenta la conexión a la base de datos.
+      $this->conn = new PDO('mysql:host=' . $this->host . ';port=' . $this->port . ';dbname=' . $this->db_name, $this->username, $this->password);
+      $this->conn->exec('set names utf8');
+    } catch (PDOException $exception) {
+      // Si hay un error en la conexión, lo muestra en pantalla. (Solo para desarrollo, en producción se debe cambiar por un mensaje genérico.)
+      echo 'Error de conexión: ' . $exception->getMessage();
+    }
+    return $this->conn;
+  }
+
+  // Función para cerrar la conexión a la base de datos.
+  public function closeConnection()
+  {
+    // Cierra la conexión a la base de datos.
+    $this->conn = null;
+  }
+
+  // Función para ejecutar una consulta a la base de datos.
+  public function executeQuery($query, $params = [])
+  {
+    // Obtiene la conexión a la base de datos.
+    $this->getConnection();
+    // Prepara la consulta a la base de datos.
+    $stmt = $this->conn->prepare($query);
+    // Ejecuta la consulta a la base de datos.
+    $stmt->execute($params);
+    // Cierra la conexión a la base de datos.
+    $this->closeConnection();
+    // Retorna el resultado de la consulta.
+    return $stmt;
+  }
+}
