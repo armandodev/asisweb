867866c922b8765f65f5ad2e293406102ca4da9e
Jorge Armando Ceras Cárdenas <jorge.armando.c.cardenas@gmail.com>
2024-02-14 23:19:55 -0600

    Se finalizo la base de la página del perfil

--- profile.php
+++ profile.php
@@ -1,11 +1,13 @@
-<?php require_once 'config/session.php'; ?>
+<?php require_once './config/session.php'; ?>
+<?php $extra_emails = $auth->getExtraEmails(); ?>
+<?php $extra_phone_numbers = $auth->getExtraPhoneNumbers(); ?>
 <!DOCTYPE html>
 <html lang="es">
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Mi perfil - Docentes CETis 121</title>
-    <link rel="icon" href="./images/favicon.webp" type="image/webp" />
+    <link rel="icon" href="./favicon.webp" type="image/webp" />
 
     <link rel="stylesheet" href="./css/output.css" />
   </head>
@@ -43,7 +45,7 @@
 
     <header
       id="header-nav"
-      class="bg-[#212121] text-white py-2 px-4 sticky top-0"
+      class="text-white py-2 px-4 fixed top-0 w-full z-50 h-[80px] flex items-center"
     >
       <div
         class="w-full m-auto max-w-screen-lg flex items-center justify-between gap-8"
@@ -65,7 +67,7 @@
         </a>
 
         <nav>
-          <ul class="flex items-center gap-2">
+          <ul class="flex items-center gap-6">
             <li
               class="border-b-2 border-transparent hover:border-gray-100 transition-colors duration-300 ease-in-out"
             >
@@ -124,6 +126,166 @@
       </div>
     </header>
 
-    <script src="./scripts/logout-modal.js"></script>
+    <main class="min-h-screen">
+      <article>
+        <img
+          class="absolute top-0 brightness-50 -z-50 h-[320px] w-full object-cover object-center"
+          src="./images/banners/banner-1.webp"
+          alt="Banner"
+        />
+        <section
+          class="h-[320px] w-full flex flex-col items-center justify-center"
+        >
+          <h1 class="text-white text-4xl font-bold text-center drop-shadow-2xl">
+            <?php echo $_SESSION['user']['first_name'] . " " . $_SESSION['user']['last_name']; ?>
+          </h1>
+          <p
+            class="text-gray-100 text-lg font-normal text-center drop-shadow-2xl"
+          >
+            <?php echo $_SESSION['user']['admin'] === 1 ? "Administrador" : "Docente" ?>
+          </p>
+        </section>
+        <section class="max-w-screen-lg m-auto py-12 px-6 min-h-[80vh]">
+          <div>
+            <div
+              class="flex items-center justify-between gap-4 mb-8 border-b-2 border-gray-300 pb-4"
+            >
+              <h2 class="text-2xl font-bold text-gray-800">
+                Información de contacto
+              </h2>
+            </div>
+
+            <div class="flex justify-between">
+              <ul class="flex flex-col gap-2 text-gray-800 text-lg font-normal">
+                Emails:
+                <li>
+                  <a
+                    class="text-blue-500 underline"
+                    href="mailto:<?php echo $_SESSION['user']['email']; ?>"
+                    ><?php echo $_SESSION['user']['email']; ?>
+                    &nbsp;(Principal)</a
+                  >
+                </li>
+                <?php if (count($extra_emails) >
+                0) { foreach ($extra_emails as $email) { ?>
+                <li>
+                  <a
+                    class="text-blue-500 underline"
+                    href="mailto:<?php echo $email; ?>"
+                    ><?php echo $email; ?></a
+                  >
+                </li>
+                <?php } } ?>
+                <li>
+                  <form
+                    class="flex gap-1"
+                    action="./auth/add-email.php"
+                    method="post"
+                  >
+                    <input
+                      type="email"
+                      name="email"
+                      id="email"
+                      class="w-full p-1 border-2 border-gray-300 rounded"
+                      placeholder="Agregar otro email"
+                    />
+                    <input
+                      type="submit"
+                      value="Agregar"
+                      class="w-fit px-1 bg-blue-500 text-white text-base rounded cursor-pointer"
+                    />
+                  </form>
+                </li>
+              </ul>
+
+              <ul class="flex flex-col gap-2 text-gray-800 text-lg font-normal">
+                Teléfonos:
+                <li>
+                  <a
+                    class="text-blue-500 underline"
+                    href="tel:<?php echo $_SESSION['user']['phone_number']; ?>"
+                    ><?php echo $_SESSION['user']['phone_number']; ?>
+                    &nbsp;(Principal)
+                  </a>
+                </li>
+                <?php if (count($extra_phone_numbers) >
+                0) { foreach ($extra_phone_numbers as $phone_number) { ?>
+                <li>
+                  <a
+                    class="text-blue-500 underline"
+                    href="tel:<?php echo $phone_number; ?>"
+                    ><?php echo $phone_number; ?></a
+                  >
+                </li>
+                <?php } } ?>
+                <li>
+                  <form
+                    class="flex gap-1"
+                    action="./auth/add-phone-number.php"
+                    method="post"
+                  >
+                    <input
+                      type="tel"
+                      name="phone_number"
+                      id="phone_number"
+                      class="w-full p-1 border-2 border-gray-300 rounded"
+                      placeholder="Agregar otro teléfono"
+                    />
+                    <input
+                      type="submit"
+                      value="Agregar"
+                      class="w-fit px-1 bg-blue-500 text-white text-base rounded cursor-pointer"
+                    />
+                  </form>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </section>
+      </article>
+    </main>
+
+    <footer class="bg-[#212121]">
+      <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <div class="sm:flex sm:items-center sm:justify-between">
+          <a
+            href="./index.php"
+            class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
+          >
+            <img src="./images/logo.webp" class="h-8" alt="DGTi Logo" />
+            <span class="text-xl font-semibold text-white">CETis 121</span>
+          </a>
+          <ul
+            class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-300 sm:mb-0"
+          >
+            <li>
+              <a href="#" class="hover:underline me-4 md:me-6">About</a>
+            </li>
+            <li>
+              <a href="#" class="hover:underline me-4 md:me-6"
+                >Privacy Policy</a
+              >
+            </li>
+            <li>
+              <a href="#" class="hover:underline me-4 md:me-6">Licensing</a>
+            </li>
+            <li>
+              <a href="#" class="hover:underline">Contact</a>
+            </li>
+          </ul>
+        </div>
+        <hr class="my-6 border-gray-500 sm:mx-auto lg:my-8" />
+        <span class="block text-sm text-gray-500 sm:text-center"
+          >©
+          <script>
+            document.write(new Date().getFullYear());
+          </script>
+          <a href="./index.php" class="hover:underline">CETis 121</a>. Todos los
+          derechos reservados.</span
+        >
+      </div>
+    </footer>
+
+    <script src="./scripts/modals.js"></script>
   </body>
 </html>
