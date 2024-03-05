9ae627a2317b5157dd3f46ceb1c7058ad77b2b86
Jorge Armando Ceras Cárdenas <jorge.armando.c.cardenas@gmail.com>
2024-03-05 13:16:14 -0600

    Correcciones minimas

--- login.php
+++ login.php
@@ -17,9 +17,9 @@
     <article class="bg-blur text-white w-[90%] max-w-xl p-8 flex flex-col gap-4 rounded-3xl rounded-bl-lg col-span-3 sm:col-span-2 z-50">
       <h1 class="text-3xl">Inicio de sesión <small class="block text-lg">Docentes <?php echo SHORT_SCHOOL_NAME ?></small></h1>
       <form class="flex flex-col items-end gap-4" action="./auth/login.php" method="post">
-        <fieldset class="flex flex-col gap-4 w-full">
+        <p class="w-full text-sm text-gray-300">Campos obligatorios <span class="text-red-500">*</span></p>
+        <fieldset class="w-full grid gap-4">
           <legend class="hidden">Información de acceso</legend>
-          <p class="text-sm text-gray-300">Campos obligatorios <span class="text-red-500">*</span></p>
 
           <label title="Correo electrónico">
             <span class="block text-lg">Correo electrónico <span class="text-red-500">*</span></span>
@@ -30,9 +30,12 @@
             <input class="w-full p-2 text-sm rounded-md text-black" type="password" name="password" placeholder="Contraseña" pattern="^.{6,100}$" minlength="6" maxlength="100" required />
           </label>
         </fieldset>
-        <input class="bg-blue-500 p-2 text-sm rounded-md cursor-pointer hover:bg-blue-700 transition-colors duration-300 ease-in-out" type="submit" value="Iniciar sesión" />
+        <input class="w-full bg-blue-500 p-2 text-sm rounded-md cursor-pointer hover:bg-blue-700 transition-colors duration-300 ease-in-out" type="submit" value="Iniciar sesión" />
       </form>
-      <p class="text-center">¿No tienes una cuenta? <a class="underline text-blue-400" href="./register.php">Solicitar registro</a></p>
+      <p class="text-center">
+        <span class="block"><a class="text-blue-400" href="./reset_password.php">¿Olvidaste tu contraseña?</a></span>
+        <span class="block">¿No tienes una cuenta? <a class="text-blue-400" href="./register.php">Solicitar registro</a></span>
+      </p>
     </article>
   </main>
 
--- register.php
+++ register.php
@@ -12,61 +12,53 @@
 </head>
 
 <body>
-  <main>
-    <article>
-      <section>
-        <h1>
-          Solicitar Registro
-          <small>Docentes
-            <?php echo SHORT_SCHOOL_NAME ?></small>
-        </h1>
-        <form action="./auth/register.php" method="post">
-          <fieldset>
-            <p>Campos obligatorios <span>*</span></p>
-            <legend>Información personal</legend>
+  <main class="min-h-screen relative flex items-center justify-center">
+    <img class="absolute top-0 left-0 w-full h-full object-cover -z-50 filter brightness-50" src="./images/banners/banner-2.webp" alt="Banner">
+    <article class="bg-blur text-white w-[90%] max-w-xl p-8 flex flex-col gap-4 rounded-3xl rounded-bl-lg col-span-3 sm:col-span-2 z-50">
+      <h1 class="text-3xl">Solicitar Registro<small class="block text-lg">Docentes <?php echo SHORT_SCHOOL_NAME ?></small></h1>
+      <form class="flex flex-col items-end gap-4" action="./auth/register.php" method="post">
+        <p class="w-full text-sm text-gray-300">Campos obligatorios <span class="text-red-500">*</span></p>
+        <fieldset class="w-full grid grid-cols-1 gap-4 sm:grid-cols-2">
+          <legend class="hidden">Nombre completo</legend>
 
-            <label title="Nombre(s)">
-              <span>Nombre(s) <span>*</span></span>
-              <input type="text" name="first_name" pattern="^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]{3,100}$" minlength="3" maxlength="100" placeholder="John Doe" required />
-            </label>
+          <label title="Nombre(s)">
+            <span class="block text-lg">Nombre(s) <span class="text-red-500">*</span></span>
+            <input class="w-full p-2 text-sm rounded-md text-black" type="text" name="first_name" pattern="^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]{3,100}$" minlength="3" maxlength="100" placeholder="John Doe" required />
+          </label>
 
-            <label title="Apellido(s)">
-              <span>Apellido(s) <span>*</span></span>
-              <input type="text" name="last_name" pattern="^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]{3,100}$" minlength="3" maxlength="100" placeholder="Doe Smith" required />
-            </label>
-          </fieldset>
+          <label title="Apellido(s)">
+            <span class="block text-lg">Apellido(s) <span class="text-red-500">*</span></span>
+            <input class="w-full p-2 text-sm rounded-md text-black" type="text" name="last_name" pattern="^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]{3,100}$" minlength="3" maxlength="100" placeholder="Doe Smith" required />
+          </label>
+        </fieldset>
 
-          <fieldset>
-            <legend>Información de contacto</legend>
+        <fieldset class="w-full grid grid-cols-1 gap-4 sm:grid-cols-2">
+          <legend class="hidden">Información de contacto</legend>
 
-            <label title="Correo electrónico">
-              <span>Correo electrónico <span>*</span></span>
-              <input type="email" name="email" pattern="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{5,255}$" minlength="5" maxlength="255" placeholder="jhondoe@gmail.com" required />
-            </label>
+          <label title="Correo electrónico">
+            <span class="block text-lg">Correo electrónico <span class="text-red-500">*</span></span>
+            <input class="w-full p-2 text-sm rounded-md text-black" type="email" name="email" pattern="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{5,255}$" minlength="5" maxlength="255" placeholder="jhondoe@gmail.com" required />
+          </label>
 
-            <label title="Teléfono">
-              <span>Teléfono <span>*</span></span>
-              <input type="tel" name="phone_number" pattern="^[0-9]{10}$" minlength="10" maxlength="10" placeholder="1234567890" required />
-            </label>
-          </fieldset>
+          <label title="Teléfono">
+            <span class="block text-lg">Teléfono <span class="text-red-500">*</span></span>
+            <input class="w-full p-2 text-sm rounded-md text-black" type="tel" name="phone_number" pattern="^[0-9]{10}$" minlength="10" maxlength="10" placeholder="1234567890" required />
+          </label>
+        </fieldset>
 
-          <fieldset>
-            <legend>Información de acceso</legend>
+        <fieldset class="w-full grid gap-4">
+          <legend class="hidden">Información de acceso</legend>
 
-            <label title="Contraseña">
-              <span>Contraseña <span>*</span></span>
-              <input type="password" name="password" pattern="^.{6,100}$" minlength="6" maxlength="100" placeholder="Contraseña" required />
-            </label>
-          </fieldset>
+          <label title="Contraseña">
+            <span class="block text-lg">Contraseña <span class="text-red-500">*</span></span>
+            <input class="w-full p-2 text-sm rounded-md text-black" type="password" name="password" pattern="^.{6,100}$" minlength="6" maxlength="100" placeholder="Contraseña" required />
+          </label>
+        </fieldset>
 
-          <input type="submit" value="Solicitar registro" />
-        </form>
+        <input class="w-full bg-blue-600 p-2 rounded-md cursor-pointer hover:bg-blue-800 transition-colors duration-300 ease-in-out" type="submit" value="Solicitar registro" />
+      </form>
 
-        <p>
-          ¿Ya tienes una cuenta?
-          <a href="./login.php">Inicia sesión</a>
-        </p>
-      </section>
+      <p class="text-center">¿Ya tienes una cuenta? <a class="text-blue-400" href="./login.php">Inicia sesión</a></p>
     </article>
   </main>
 </body>
