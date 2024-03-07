e067b8484fd96cda4b0c2ec1c8a07508d2618984
Jorge Armando Ceras Cárdenas <jorge.armando.c.cardenas@gmail.com>
2024-03-07 10:13:26 -0600

    Estilos base de la navegacion del dashboard

--- admin/components/header.php
+++ admin/components/header.php
@@ -1,14 +1,14 @@
-<header>
-  <h1>Dashboard</h1>
+<header class="w-60 h-screen fixed top-0 left-0">
+  <h1 class="bg-[#202020] text-white text-lg p-2 h-10">Dashboard</h1>
 
-  <nav>
-    <ul>
+  <nav class="border-r-2 border-gray-400 h-full">
+    <ul class="bg-[#dfdfdf] flex items-center justify-center gap-2 p-2 text-base">
       <li><a href="./../index.php">Inicio</a></li>
       <li><a href="./config.php">Parámetros</a></li>
       <li><a href="./">Actualizar</a></li>
     </ul>
 
-    <ul>
+    <ul class="overflow-auto p-4 text-gray-800 text-base font-semibold flex flex-col gap-2">
       <li><a href="./users.php" title="Ir a Usuarios">Usuarios</a></li>
       <li><a href="./students.php" title="Ir a Estudiantes">Estudiantes</a></li>
       <li><a href="./groups.php" title="Ir a Grupos">Grupos</a></li>
@@ -17,3 +17,8 @@
     </ul>
   </nav>
 </header>
+<style>
+  body {
+    margin-left: 15rem;
+  }
+</style>
