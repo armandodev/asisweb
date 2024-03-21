a052bcdb6c2b5dbadb7c21a096ef8a0e800fd1e9
armandodev <jorge.armando.c.cardenas@gmail.com>
2024-03-20 23:47:49 -0600

    Refactor logout.php to handle session expiration and display appropriate messages

--- auth/logout.php
+++ auth/logout.php
@@ -6,11 +6,19 @@ unset($_SESSION['user']);
 // Destruye la sesión.
 session_destroy();
 
+session_start();
+
 // Si se ha recibido un parámetro 'expired' y su valor es 1, redirige al usuario a la página de inicio de sesión con un mensaje de error.
-$path = isset($_GET['expired']) && $_GET['expired'] == 1
-  ? "../login.php?error=expired"
-  : "../login.php?success=logout";
+isset($_GET['expired']) && $_GET['expired'] == 1
+  ? $_SESSION['message'] = [
+    'type' => 'error',
+    'content' => 'La sesión ha expirado. Por favor, inicia sesión de nuevo.'
+  ]
+  : $_SESSION['message'] = [
+    'type' => 'success',
+    'content' => 'Has cerrado sesión correctamente, ¡hasta pronto!'
+  ];
 
 // Redirige al usuario a la página de inicio de sesión.
-header('Location: ' . $path);
+header('Location: ./../index.php');
 exit;
