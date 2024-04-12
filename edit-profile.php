c051f5f05a17b15be61959b52cf583b2b9da7f34
Jorge Armando Ceras Cárdenas <jorge.armando.c.cardenas@gmail.com>
2024-04-11 21:31:56 -0600

    Agregue estilos al archivo de editar perfil

--- edit-profile.php
+++ edit-profile.php
@@ -15,39 +15,37 @@ if (!isset($_SESSION['user'])) {
   <title>Edita tu perfil | Docentes CETis 121</title>
   <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
 
-  <link rel="stylesheet" href="./css/normalize.css">
-  <link rel="stylesheet" href="./css/styles.css">
-  <link rel="stylesheet" href="./css/form.css">
+  <link rel="stylesheet" href="./css/output.css">
 </head>
 
 <body>
   <main>
-    <article id="form-article" class="container">
+    <article class="container min-h-screen flex gap-4 flex-col justify-center items-center">
       <section>
-        <h1>Edita tu perfil <small>Docentes CETis 121</small></h1>
+        <h1 class="text-3xl text-center font-semibold">Edita tu perfil <small class="block text-xl font-normal text-[#a91f21]">Docentes CETis 121</small></h1>
       </section>
       <section>
-        <form action="./api/auth/edit-profile.php" method="post">
-          <p>Campos obligatorios <span>*</span></p>
+        <form class="flex gap-4 flex-col justify-center w-full max-w-screen-sm text-lg" action="./api/auth/edit-profile.php" method="post">
+          <p class="text-normal">Campos obligatorios <span class="text-red-600">*</span></p>
           <fieldset>
-            <legend>Datos de contacto</legend>
+            <legend hidden>Datos de contacto</legend>
 
             <label title="Correo electrónico">
-              <span>Correo electrónico <span>*</span></span>
-              <input type="email" name="email" autocomplete="email" pattern="^(?=.{5,255}$)[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$" minlength="5" maxlength="255" placeholder="ejemplo@dominio.com" required value="<?= $_SESSION['user']['email'] ?>" />
+              <span>Correo electrónico <span class="text-red-600">*</span></span>
+              <input class="input" type="email" name="email" autocomplete="email" pattern="^(?=.{5,255}$)[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$" minlength="5" maxlength="255" placeholder="ejemplo@dominio.com" required value="<?= $_SESSION['user']['email'] ?>" />
             </label>
 
             <label title="Teléfono">
-              <span>Teléfono <span>*</span></span>
-              <input type="tel" name="tel" autocomplete="tel" pattern="^[0-9 ]{10,15}$" minlength="10" maxlength="15" placeholder="353 000 0000" required value="<?= $_SESSION['user']['tel'] ?>" />
+              <span>Teléfono <span class="text-red-600">*</span></span>
+              <input class="input" type="tel" name="tel" autocomplete="tel" pattern="^[0-9 ]{10,15}$" minlength="10" maxlength="15" placeholder="353 000 0000" required value="<?= $_SESSION['user']['tel'] ?>" />
             </label>
           </fieldset>
 
-          <input type="submit" value="Guardar cambios" />
+          <input class="button" type="submit" value="Guardar cambios" />
         </form>
       </section>
       <ul>
-        <li><a href="./profile.php">Regresar a tu perfil</a></li>
+        <li><a class="text-lg text-[#a91f21] underline" href="./profile.php">Regresar a tu perfil</a></li>
       </ul>
     </article>
   </main>
