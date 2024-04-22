81e11c05749dffc9bd86ffe6cc5328bc31860a1a
Jorge Armando Ceras Cárdenas <jorge.armando.c.cardenas@gmail.com>
2024-04-21 21:18:18 -0600

    Estilos

--- register.php
+++ register.php
@@ -15,45 +15,43 @@ if (isset($_SESSION['user'])) {
   <title>Registro | Docentes CETis 121</title>
   <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
 
-  <link rel="stylesheet" href="./css/normalize.css">
-  <link rel="stylesheet" href="./css/styles.css">
-  <link rel="stylesheet" href="./css/form.css">
+  <link rel="stylesheet" href="./css/output.css">
 </head>
 
 <body>
   <main>
-    <article id="form-article" class="container">
-      <section>
-        <h1>Solicitar Registro <small>Docentes CETis 121</small></h1>
+    <article class="container min-h-screen flex gap-4 flex-col justify-center items-center">
+      <section class="w-full max-w-screen-sm">
+        <h1 class="text-3xl text-center font-semibold">Solicitar Registro <small class="block text-xl font-normal text-[#a91f21]">Docentes CETis 121</small></h1>
       </section>
-      <section>
-        <form action="./api/auth/register.php" method="post">
-          <p>Campos obligatorios <span>*</span></p>
-          <fieldset class="grid-span-2">
+      <section class="w-full max-w-screen-sm">
+        <form class="flex gap-4 flex-col justify-center w-full text-lg" action="./api/auth/register.php" method="post">
+          <p class="text-base">Campos obligatorios <span class="text-red-600">*</span></p>
+          <fieldset class="grid grid-cols-1 sm:grid-cols-2 sm:gap-4">
             <legend hidden>Nombre completo</legend>
 
             <label title="Nombre(s)">
               <span>Nombre(s) <span>*</span></span>
-              <input type="text" name="first_name" autocomplete="given-name" pattern="^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]{3,100}$" minlength="3" maxlength="100" placeholder="John" required />
+              <input class="input" type="text" name="first_name" autocomplete="given-name" pattern="^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]{3,100}$" minlength="3" maxlength="100" placeholder="John" required />
             </label>
 
             <label title="Apellido(s)">
               <span>Apellido(s) <span>*</span></span>
-              <input type="text" name="last_name" autocomplete="family-name" pattern="^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]{3,100}$" minlength="3" maxlength="100" placeholder="Doe" required />
+              <input class="input" type="text" name="last_name" autocomplete="family-name" pattern="^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]{3,100}$" minlength="3" maxlength="100" placeholder="Doe" required />
             </label>
           </fieldset>
 
-          <fieldset>
+          <fieldset class="grid grid-cols-1 sm:grid-cols-2 sm:gap-4">
             <legend hidden>Datos de contacto</legend>
 
             <label title="Correo electrónico">
               <span>Correo electrónico <span>*</span></span>
-              <input type="email" name="email" autocomplete="email" pattern="^(?=.{5,255}$)[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$" minlength="5" maxlength="255" placeholder="ejemplo@dominio.com" required />
+              <input class="input" type="email" name="email" autocomplete="email" pattern="^(?=.{5,255}$)[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$" minlength="5" maxlength="255" placeholder="ejemplo@dominio.com" required />
             </label>
 
             <label title="Teléfono">
               <span>Teléfono <span>*</span></span>
-              <input type="tel" name="tel" autocomplete="tel" pattern="^[0-9 ]{10,15}$" minlength="10" maxlength="15" placeholder="353 000 0000" required />
+              <input class="input" type="tel" name="tel" autocomplete="tel" pattern="^[0-9 ]{10,15}$" minlength="10" maxlength="15" placeholder="353 000 0000" required />
             </label>
           </fieldset>
 
@@ -62,15 +60,15 @@ if (isset($_SESSION['user'])) {
 
             <label title="Contraseña">
               <span>Contraseña <span>*</span></span>
-              <input type="password" name="password" autocomplete="new-password" pattern="^.{6,100}$" minlength="6" maxlength="100" placeholder="********" required />
+              <input class="input" type="password" name="password" autocomplete="new-password" pattern="^.{6,100}$" minlength="6" maxlength="100" placeholder="********" required />
             </label>
           </fieldset>
 
-          <input type="submit" value="Solicitar registro" />
+          <input class="button" type="submit" value="Solicitar registro" />
         </form>
       </section>
-      <ul>
-        <li>¿Ya tienes una cuenta? <a href="./index.php">Inicia sesión</a></li>
+      <ul class="text-center">
+        <li>¿Ya tienes una cuenta? <a class="text-lg text-[#a91f21] underline" href="./index.php">Inicia sesión</a></li>
       </ul>
     </article>
   </main>
