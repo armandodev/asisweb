ed245e1c3dbda9f308a1d58c6f982c9508250f30
Jorge Armando Ceras Cárdenas <jorge.armando.c.cardenas@gmail.com>
2024-05-04 16:59:36 -0600

    Elimine este archivo re innecesario (no lo revises :D)

--- js/search-form.js
+++
@@ -1,8 +0,0 @@
-const $form = document.getElementById("search-form");
-const $input = $form.querySelector("input");
-
-$form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const search = $input.value.trim();
-  window.location.href = `./subjects.php?search=${search}`;
-});
