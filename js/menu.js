c3223137c7edd220fe672304e15899bdbae0c947
Jorge Armando Ceras Cárdenas <jorge.armando.c.cardenas@gmail.com>
2024-06-08 18:56:08 -0600

    Agregamos comentarios a los archivos de js

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,10 @@
-const toggleMenu = document.getElementById("toggle-menu");
-const menu = document.getElementById("menu");
+const toggleMenu = document.getElementById("toggle-menu"); // Seleccionamos el botón del menú
+const menu = document.getElementById("menu"); // Seleccionamos el menú
 
 toggleMenu.addEventListener("click", () => {
-  menu.classList.toggle("active");
-  toggleMenu.innerHTML = menu.classList.contains("active")
-    ? "<img src='./icons/close.svg' alt='Cerrar menú'>"
-    : "<img src='./icons/menu.svg' alt='Abrir menú'>";
+  // Al hacer clic en el botón del menú
+  menu.classList.toggle("active"); // Colocamos o quitamos la clase active del menú
+  toggleMenu.innerHTML = menu.classList.contains("active") // Cambiamos el texto del botón del menú
+    ? "<img src='./icons/close.svg' alt='Cerrar menú'>" // Si el menú está activo, colocamos el icono de cerrar
+    : "<img src='./icons/menu.svg' alt='Abrir menú'>"; // Si el menú está inactivo, colocamos el icono de abrir
 });
--- js/modals.js
+++ js/modals.js
@@ -1,57 +1,73 @@
-const welcome = document.getElementById("welcome-modal");
+// Modal de bienvenida
+const welcome = document.getElementById("welcome-modal"); // Seleccionamos el elemento del modal
 
 if (welcome) {
-  const closeWelcome = document.getElementById("close-welcome-modal");
+  // Si el elemento del modal existe
+  const closeWelcome = document.getElementById("close-welcome-modal"); // Seleccionamos el botón de cerrar el modal
 
   if (closeWelcome) {
+    // Si el botón de cerrar el modal existe
     closeWelcome.addEventListener("click", () => {
-      welcome.close();
+      // Al hacer clic en el botón de cerrar el modal
+      welcome.close(); // Cerramos el modal
     });
   }
 
-  welcome.showModal();
+  welcome.showModal(); // Mostramos el modal al cargar la página
 }
 
-const logout = document.getElementById("logout-modal");
-const showLogout = document.getElementById("logout");
-const closeLogout = document.getElementById("close-logout-modal");
+// Modal de logout
+const logout = document.getElementById("logout-modal"); // Seleccionamos el elemento del modal
+const showLogout = document.getElementById("logout"); // Seleccionamos el botón de abrir el modal
+const closeLogout = document.getElementById("close-logout-modal"); // Seleccionamos el botón de cerrar el modal
 
 if (logout && showLogout && closeLogout) {
+  // Si el elemento del modal existe, asi como los botones de abrir y cerrar el modal
   showLogout.addEventListener("click", () => {
-    logout.showModal();
+    // Al hacer clic en el botón de abrir el modal
+    logout.showModal(); // Mostramos el modal
   });
 
   closeLogout.addEventListener("click", () => {
-    logout.close();
+    // Al hacer clic en el botón de cerrar el modal
+    logout.close(); // Cerramos el modal
   });
 }
 
-const editProfile = document.getElementById("edit-profile-modal");
-const closeEditProfile = document.getElementById("close-edit-profile-modal");
-const showEditProfile = document.getElementById("edit-profile");
+// Modal de editar perfil
+const editProfile = document.getElementById("edit-profile-modal"); // Seleccionamos el elemento del modal
+const closeEditProfile = document.getElementById("close-edit-profile-modal"); // Seleccionamos el botón de cerrar el modal
+const showEditProfile = document.getElementById("edit-profile"); // Seleccionamos el botón de abrir el modal
 
 if (editProfile && showEditProfile && closeEditProfile) {
+  // Si el elemento del modal existe, asi como los botones de abrir y cerrar el modal
   showEditProfile.addEventListener("click", () => {
-    editProfile.showModal();
+    // Al hacer clic en el botón de abrir el modal
+    editProfile.showModal(); // Mostramos el modal
   });
 
   closeEditProfile.addEventListener("click", () => {
-    editProfile.close();
+    // Al hacer clic en el botón de cerrar el modal
+    editProfile.close(); // Cerramos el modal
   });
 }
 
-const editPassword = document.getElementById("edit-password-modal");
-const closeEditPassword = document.getElementById("close-edit-password-modal");
-const showEditPassword = document.getElementById("edit-password");
+// Modal de editar contraseña
+const editPassword = document.getElementById("edit-password-modal"); // Seleccionamos el elemento del modal
+const closeEditPassword = document.getElementById("close-edit-password-modal"); // Seleccionamos el botón de cerrar el modal
+const showEditPassword = document.getElementById("edit-password"); // Seleccionamos el botón de abrir el modal
 
 if (editPassword && showEditPassword && closeEditPassword) {
+  // Si el elemento del modal existe, asi como los botones de abrir y cerrar el modal
   showEditPassword.addEventListener("click", () => {
-    if (editProfile) editProfile.close();
-
-    editPassword.showModal();
+    // Al hacer clic en el botón de abrir el modal
+    if (editProfile) editProfile.close(); // Si el modal de editar perfil existe, aseguramos de cerrarlo
+    editPassword.showModal(); // Mostramos el modal
   });
 
   closeEditPassword.addEventListener("click", () => {
-    editPassword.close();
+    // Al hacer clic en el botón de cerrar el modal
+    editPassword.close(); // Cerramos el modal
+    if (editProfile) editProfile.showModal(); // Si el modal de editar perfil existe, aseguramos de mostrarlo
   });
 }
